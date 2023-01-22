6935ac738716b6a03d2ce66c41eeffc5f1f520e4
AdministratorO5 <beka.baktygulov@gmail.com>
2023-01-22 18:40:43 +0300

    Standard methods

--- script.js
+++ script.js
@@ -1523,10 +1523,385 @@
 // }
 // console.log(obj)
 
+//-----Многомерные массивы -------
 
+// let arr = [
+// 	['a', 'b', 'c'],
+// 	['d', 'e', 'f'],
+// 	['g', 'h', 'i'],
+// 	['j', 'k', 'l'],
+// ];
+// console.log( arr[1] [1] , arr[3][2], arr[2][0], arr[0][0]);
+//---нашли сумму двумерного массива--
+// let arr = [[1, 2], [3, 4], [5, 6]];
+// let sum =0 ;
+// sum =(arr[0][0]+arr[0][1]+arr[1][0]+arr[1][1]+arr[2][0]+arr[2][1])
+// console.log(sum)
+//--- трехмерный массив и его сумма ---
+// let arr = [	[	[1, 2],	[3, 4],],  	[	[5, 6],	[7, 8],],];
+// let sum= 0;
+// sum = ( arr[0][0][0]+arr[0][0][1]+arr[0][1][0]+arr[0][1][1] + arr[1][0][0]+arr[1][0][1]+arr[1][1][0]+arr[1][1][1])
+// console.log( sum)
+// ----вывод всех элементов многомерного массива с фунцией и рекурсией--
+// let arr = [[1, 2, 3, [4, 5, [6, 7]]], [8, [9, 10]]];
+// function foreachArr(arr) { //в ф-цию передаем массив
+// arr.forEach(function(item) { //перебираем массив
+//       //если элемент массива является вложенным массивом - вызываем нашу ф-цию (будет рекурсия)
+// if (Array.isArray(item)) foreachArr(item);
+// else document.write(item); //иначе - выводим элемент
+//  });
+// }
+//   foreachArr(arr);
+//---- сумма всех элементов многомерного массива без РЕКУРСИИ----
+// let arr = [[1, 2, 3, [4, 5, [6, 7]]], [8, [9, 10]]];
+// let sum = 0;
+// while (arr.length) {
+//   let cur = arr.shift();
+//   if (Array.isArray(cur)) {
+//     arr.unshift(...cur);
+//     continue;
+//   }
+//   sum += cur;
+// }
+// console.log(sum);
+//-----заполнение массивов циклом
+// let arr= [];
+// for ( let i =0; i<3;i++){
+//   arr[i]= [];
+// for ( let j =0;j<5;j++){
+//   arr[i].push(j+1);
+// }
+// }
+// console.log( arr )
+//----- заполнение массивов строками----
+// let arr= [];
+// let t = "x";
+// for ( let i =0; i<3;i++){
+//   arr[i]= [];
+// for ( let j =0;j<5;j++){
+//   arr[i].push(t);
+// }
+// }
+// console.log( arr )
+//-----заполнение многомерного масссива по порядку
+// let arr = [];
+// for (let i = 0, k = 1; i < 3; 
+// 	i++) { 
+// 	arr[i] = [];
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i].push(k++);
+// 	}
+// }
+// console.log(arr);
 
+// let arr = [];
+// let k = 1; // счетчик
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = [];
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i].push(k); // записываем счетчик
+// 		k++; // увеличиваем счетчик
+// 	}
+// }
+// console.log(arr);
+// let arr = [];
+// for (let i = 0, k = 1; i < 4; 
+// 	i++) { 
+// 	arr[i] = [];
+// 	for (let j = 0; j < 2; j++) {
+// 		arr[i].push(k++);
+// 	}
+// }
+// console.log(arr);
+//--
+// let arr = [];
+// for (let i = 0,k=1 ; i < 3; i++) {
+// 	arr[i] = [];
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i].push(k++);
+// 	}
+// }
+// console.log(arr);
+// let arr = [];
+// let k = 1;
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = [];
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i][j] = k++;
+// 	}
+// }
+// console.log(arr);
+// let arr = [];
+// let k = 1;
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = [];
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i][j] = k;
+// 		k++;
+// 	}
+// }
+// console.log(arr);
+//-----перебор многомерного массива и нахождение ее суммы
+// let obj = {
+// 	key1: {a: 1, b: 2, c: {d: 3,e: 4,}, f: 5,},
+// 	key2: {g: 6, h: 7,},}
+// console.log(obj.key1['a']+obj.key1['b']+obj.key1['c']['d']+obj.key1['c']['e']+obj.key1['f']+ obj.key2['g']+obj.key2['h'])
+//-----сумма  элементов многомерного обьекта с циклами
+// let obj = {
+// 	key1: {
+// 		key1: 1,
+// 		key2: 2,
+// 		key3: 3,
+// 	},
+// 	key2: {
+// 		key1: 4,
+// 		key2: 5,
+// 		key3: 6,
+// 	},
+// 	key3: {
+// 		key1: 7,
+// 		key2: 8,
+// 		key3: 9,
+// 	},
+// }
+// let sum =0 ;
+// for ( let key in obj ){
+//   let subObj = obj[key];
+//   for (let val in subObj){
+//     console.log(subObj[val])  // Если убрать это то выведет только сумму всех обьектов .
+//     sum += subObj[val]
+//   }
+// }
+// console.log(sum)
 
+// let students = {
+    // 	'group1': ['name11', 'name12', 
+    // 		'name13'], 
+    // 	'group2': ['name21', 'name22', 
+    // 		'name23'], 
+    // 	'group3': ['name31', 'name32', 
+    // 		'name33'], 
+    // };
+    // console.log( students ['group3'][0]);
 
+    // let data = {
+    //     1: [
+    //         'data11',
+    //         'data12',
+    //         'data13',
+    //     ],
+    //     2: [
+    //         'data21',
+    //         'data22',
+    //         'data23',
+    //     ],
+    //     3: [
+    //         'data31',
+    //         'data32',
+    //         'data33',
+    //     ],
+    //     4: [
+    //         'data41',
+    //         'data42',
+    //         'data43',
+    //     ],
+    // };
+    // for ( let date in data){
+    //   for ( let dot of data[date]){
+    //     console.log(dot)
+        
+    //   }
+    // }
+//   ----- спомошью трех вложенных циклов вывели строки на консоль
+    // let data = [
+    //     {
+    //         1: [
+    //             'data111',
+    //             'data112',
+    //             'data113',
+    //         ],
+    //         2: [
+    //             'data121',
+    //             'data122',
+    //             'data123',
+    //         ],
+    //     },
+    //     {
+    //         1: [
+    //             'data211',
+    //             'data212',
+    //             'data213',
+    //         ],
+    //         2: [
+    //             'data221',
+    //             'data222',
+    //             'data223',
+    //         ],
+    //     },
+    //     {
+    //         1: [
+    //             'data411',
+    //             'data412',
+    //             'data413',
+    //         ],
+    //         2: [
+    //             'data421',
+    //             'data422',
+    //             'data423',
+    //         ],
+    //     },
+    // ];
+    // for ( let date of data){
+    //   for ( let key in date){
+    //     for ( let obj of date[key])
+    //     console.log(obj)
+    //   }
+    // }
+// ------- сумма зрплат и перебор только одни цмклом посколку обьект внутри массива 
+
+    // let employees = [
+        // 	{
+        // 		name: 'name1',
+        // 		salary: 300,
+        // 	},
+        // 	{
+        // 		name: 'name2',
+        // 		salary: 400,
+        // 	},
+        // 	{
+        // 		name: 'name3',
+        // 		salary: 500,
+        // 	},
+        // ]
+        // let sum =0 ;
+        // for ( let user of employees){
+        //   sum += user.salary
+        // }console.log ( sum )
+// ------ тут мы через иф выделили тех кому больще 30 и вывели сумму их зарплат.
+        // let employees = [
+        //     {
+        //         name: 'name1',
+        //         salary: 300,
+        //         age: 28,
+        //     },
+        //     {
+        //         name: 'name2',
+        //         salary: 400,
+        //         age: 29,
+        //     },
+        //     {
+        //         name: 'name3',
+        //         salary: 500,
+        //         age: 30,
+        //     },
+        //     {
+        //         name: 'name4',
+        //         salary: 600,
+        //         age: 31,
+        //     },
+        //     {
+        //         name: 'name5',
+        //         salary: 700,
+        //         age: 32,
+        //     },
+        // ];
+        // let sum =0 ;
+        // let res = [] ;
+        // for ( let user of employees){
+        //   if ( user.age >= 30){
+        //     sum += user.salary
+        
+        //   }
+        // }console.log ( sum )
+//---------- 
+        // let months = {
+            // 	'ru': [
+            // 		'январь',
+            // 		'февраль',
+            // 		'март',
+            // 		'апрель',
+            // 		'май',
+            // 		'июнь',
+            // 		'июль',
+            // 		'август',
+            // 		'сентябрь',
+            // 		'октябрь',
+            // 		'ноябрь',
+            // 		'декабрь',
+            // 	],
+            // 	'en': [
+            // 		'january',
+            // 		'february',
+            // 		'march',
+            // 		'april',
+            // 		'may',
+            // 		'june',
+            // 		'july',
+            // 		'august',
+            // 		'september',
+            // 		'october',
+            // 		'november',
+            // 		'december',
+            // 	],
+            // };
+            // let lang = 'en';
+            // let month = 5 ;
+            // console.log(months[lang][month] )
+        
+            // let affairs = {
+                // 	'2018': {
+                // 		11: {
+                // 			29: ['дело111', 'дело112', 'дело113'],
+                // 			30: ['дело121', 'дело122', 'дело123'],
+                // 		},
+                // 		12: {
+                // 			30: ['дело211', 'дело212', 'дело213'],
+                // 			31: ['дело221', 'дело222', 'дело223'],
+                // 		},
+                // 	},
+                // 	'2019': {
+                // 		12: {
+                // 			29: ['дело311', 'дело312', 'дело313'],
+                // 			30: ['дело321', 'дело322', 'дело323'],
+                // 			31: ['дело331', 'дело332', 'дело333'],
+                // 		}
+                // 	},
+                // }
+                // let day = '30';
+                // let month ='12';
+                // let year ='2019';
+                // let doc = 2;
+                // console.log( affairs[year][month][day][doc])
+
+                // let employees = [
+                //     {
+                //       name: 'name1',
+                //       salary: 300,
+                //       age:28,
+                //     },
+                //     {
+                //       name: 'name2',
+                //       salary: 400,
+                //       age: 29,
+                //     },
+                //     {
+                //       name: 'name3',
+                //       salary: 500,
+                //       age:30,
+                //     },
+                //   ];
+                //   employees.push( { 
+                //   name: 'name4',
+                //     salary: 600,
+                //     age: 31,
+                //   });
+                //   console.log(employees)
+
+//Стандартные методы--------------------------
 
 
 
